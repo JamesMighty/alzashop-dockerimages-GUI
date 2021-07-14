b51607642d4d50e3a63062232c064017efc45497
JamesMighty <41427022+JamesMighty@users.noreply.github.com>
2021-07-14 07:38:51 +0200

    update

--- test.sh
+++ test.sh
@@ -1,8 +1,9 @@
-(echo "testing inside" & \
-cat /startup.sh & \
-cat /etc/supervisor/supervisord.conf & \
-geckodriver --version & \
-python3 --version & \
-pip --version & \
-firefox -width=1080 -height=720 -url http://google.com & \
+(echo "testing inside container" && \
+cat /startup.sh && \
+cat /etc/supervisor/supervisord.conf && \
+cat /etc/supervisor/conf.d/supervisord.conf && \
+geckodriver --version && \
+python3 --version && \
+pip --version && \
+firefox -width=1080 -height=720 -url http://google.com && \
 exit)
